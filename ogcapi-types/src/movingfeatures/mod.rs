99072bf537a1de088f4f8e75b6a8d89cf2b0add7
Matthias Kraus <40459408+KrausMatthias@users.noreply.github.com>
2025-05-24 10:13:49 +0200

    Refactor and comment temporal properties

--- ogcapi-types/src/movingfeatures/mod.rs
+++ ogcapi-types/src/movingfeatures/mod.rs
@@ -1,11 +1,10 @@
 mod crs;
+pub mod temporal_complex_geometry;
 pub mod temporal_geometry;
 pub mod temporal_primitive_geometry;
-pub mod temporal_complex_geometry;
 
 pub mod temporal_properties;
+pub mod mfjson_temporal_properties;
 pub mod temporal_property;
-pub mod temporal_primitive_value;
-pub mod temporal_properties_list;
 
 pub mod trs;
