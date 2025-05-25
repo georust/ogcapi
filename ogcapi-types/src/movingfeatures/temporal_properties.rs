98064b09f286f01db3e34df2168eabe276153117
Matthias Kraus <40459408+KrausMatthias@users.noreply.github.com>
2025-05-25 14:45:45 +0200

    Add test to temporal properties and fix some bugs

--- ogcapi-types/src/movingfeatures/temporal_properties.rs
+++ ogcapi-types/src/movingfeatures/temporal_properties.rs
@@ -2,7 +2,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::common::Links;
 
-use super::{mfjson_temporal_properties::MFJsonTemporalProperties, temporal_property::TemporalProperty};
+use super::{
+    mfjson_temporal_properties::MFJsonTemporalProperties, temporal_property::TemporalProperty,
+};
 
 /// A TemporalProperties object consists of the set of [TemporalProperty] or a set of [MFJsonTemporalProperties].
 ///
@@ -26,3 +28,117 @@ pub enum TemporalPropertiesValue {
     TemporalProperty(Vec<TemporalProperty>),
 }
 
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{
+        common::Link,
+        movingfeatures::{
+            mfjson_temporal_properties::ParametricValues, temporal_property::Interpolation,
+        },
+    };
+
+    use super::*;
+
+    #[test]
+    fn serde_temporal_properties() {
+        let links: Links = vec![
+            Link::new("https://data.example.org/collections/mfc-1/items/mf-1/tproperties","self").mediatype("application/json"),
+            Link::new("https://data.example.org/collections/mfc-1/items/mf-1/tproperties&offset=2&limit=2","next").mediatype("application/json"),
+        ];
+
+        let temporal_properties = TemporalProperties {
+            temporal_properties: TemporalPropertiesValue::MFJsonTemporalProperties(vec![
+                MFJsonTemporalProperties {
+                    datetimes: vec![
+                        // TODO does type actually need to be UTC or could it be FixedOffset
+                        // aswell? Converting to UTC loses the information of the original offset!
+                        chrono::DateTime::parse_from_rfc3339("2011-07-14T22:01:06.000Z")
+                            .unwrap()
+                            .into(),
+                        chrono::DateTime::parse_from_rfc3339("2011-07-14T22:01:07.000Z")
+                            .unwrap()
+                            .into(),
+                        chrono::DateTime::parse_from_rfc3339("2011-07-14T22:01:08.000Z")
+                            .unwrap()
+                            .into(),
+                    ],
+                    values: HashMap::from([
+                        (
+                            "length".to_string(),
+                            ParametricValues::Measure {
+                                values: vec![1.0, 2.4, 1.0],
+                                interpolation: Some(Interpolation::Linear),
+                                description: None,
+                                form: Some("http://qudt.org/vocab/quantitykind/Length".to_string()),
+                            },
+                        ),
+                        (
+                            "speed".to_string(),
+                            ParametricValues::Measure {
+                                values: vec![65.0, 70.0, 80.0],
+                                interpolation: Some(Interpolation::Linear),
+                                form: Some("KMH".to_string()),
+                                description: None
+                            },
+                        ),
+                    ]),
+                },
+            ]),
+            links: Some(links),
+            time_stamp: Some("2021-09-01T12:00:00Z".into()),
+            number_matched: Some(10),
+            number_returned: Some(2),
+        };
+
+        // https://developer.ogc.org/api/movingfeatures/index.html#tag/TemporalProperty/operation/retrieveTemporalProperties
+        let tp_json = r#"{
+          "temporalProperties": [
+            {
+              "datetimes": [
+                "2011-07-14T22:01:06.000Z",
+                "2011-07-14T22:01:07.000Z",
+                "2011-07-14T22:01:08.000Z"
+              ],
+              "length": {
+                "type": "Measure",
+                "form": "http://qudt.org/vocab/quantitykind/Length",
+                "values": [
+                  1,
+                  2.4,
+                  1
+                ],
+                "interpolation": "Linear"
+              },
+              "speed": {
+                "type": "Measure",
+                "form": "KMH",
+                "values": [
+                  65,
+                  70,
+                  80
+                ],
+                "interpolation": "Linear"
+              }
+            }
+          ],
+          "links": [
+            {
+              "href": "https://data.example.org/collections/mfc-1/items/mf-1/tproperties",
+              "rel": "self",
+              "type": "application/json"
+            },
+            {
+              "href": "https://data.example.org/collections/mfc-1/items/mf-1/tproperties&offset=2&limit=2",
+              "rel": "next",
+              "type": "application/json"
+            }
+          ],
+          "timeStamp": "2021-09-01T12:00:00Z",
+          "numberMatched": 10,
+          "numberReturned": 2
+        }"#;
+        assert_eq!(temporal_properties, serde_json::from_str(tp_json).unwrap());
+    }
+}
--- ogcapi-types/src/movingfeatures/temporal_property.rs
+++ ogcapi-types/src/movingfeatures/temporal_property.rs
@@ -3,46 +3,36 @@ use serde::{Deserialize, Serialize};
 
 use crate::common::Links;
 
-
-/// A temporal property object is a collection of dynamic non-spatial attributes and their temporal values with time. 
-/// An abbreviated copy of this information is returned for each TemporalProperty in the 
+/// A temporal property object is a collection of dynamic non-spatial attributes and their temporal values with time.
+/// An abbreviated copy of this information is returned for each TemporalProperty in the
 /// [{root}/collections/{collectionId}/items/{mFeatureId}/tproperties](super::temporal_properties::TemporalProperties) response.
 /// The schema for the temporal property object presented in this clause is an extension of the [ParametricValues Object](https://docs.opengeospatial.org/is/19-045r3/19-045r3.html#tproperties) defined in [MF-JSON](https://docs.ogc.org/is/22-003r3/22-003r3.html#OGC_19-045r3).
 ///
 /// See [8.9. TemporalProperty](https://docs.ogc.org/is/22-003r3/22-003r3.html#resource-temporalProperty-section)
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct TemporalProperty {
     /// An identifier for the resource assigned by an external entity.
-    name: String,
-    value_sequence: TemporalPropertyValue,
+    pub name: String,
+    #[serde(flatten)]
+    pub value_sequence: TemporalPropertyValue,
     /// A unit of measure
-    form: Option<String>,
+    pub form: Option<String>,
     /// A short description
-    description: Option<String>,
-    links: Option<Links>
+    pub description: Option<String>,
+    pub links: Option<Links>,
 }
 
 /// A predefined temporal property type.
 ///
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-#[serde(tag = "type")]
-#[serde(rename_all = "camelCase")]
+#[serde(tag = "type", content = "valueSequence" )]
 pub enum TemporalPropertyValue {
-    TBoolean {
-        value_sequence: Vec<TemporalPrimitiveValue<bool>>,
-    },
-    TText {
-        value_sequence: Vec<TemporalPrimitiveValue<String>>,
-    },
-    TInteger {
-        value_sequence: Vec<TemporalPrimitiveValue<i64>>,
-    },
-    TReal {
-        value_sequence: Vec<TemporalPrimitiveValue<f64>>,
-    },
-    TImage {
-        value_sequence: Vec<TemporalPrimitiveValue<String>>,
-    },
+    TBoolean(Vec<TemporalPrimitiveValue<bool>>),
+    TText(Vec<TemporalPrimitiveValue<String>>),
+    TInteger(Vec<TemporalPrimitiveValue<i64>>),
+    TReal(Vec<TemporalPrimitiveValue<f64>>),
+    TImage(Vec<TemporalPrimitiveValue<String>>),
 }
 
 /// The TemporalPrimitiveValue resource represents the dynamic change of a non-spatial attribute’s value with time. An
@@ -52,31 +42,70 @@ pub enum TemporalPropertyValue {
 /// See [8.10. TemporalPrimitiveValue](https://docs.ogc.org/is/22-003r3/22-003r3.html#resource-temporalPrimitiveValue-section)
 #[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
 pub struct TemporalPrimitiveValue<T> {
-    id: String,
-    datetimes: Vec<DateTime<Utc>>,
-    values: Vec<T>,
-    interpolation: Interpolation,
+    /// A unique identifier to the temporal primitive value. 
+    // TODO mandatory according to https://docs.ogc.org/is/22-003r3/22-003r3.html#_overview_13
+    // but missing in response sample at https://developer.ogc.org/api/movingfeatures/index.html#tag/TemporalProperty/operation/retrieveTemporalProperty
+    pub id: Option<String>,
+    /// A sequence of monotonic increasing instants.
+    pub datetimes: Vec<DateTime<Utc>>,
+    /// A sequence of dynamic values having the same number of elements as “datetimes”.
+    // TODO enforce length
+    pub values: Vec<T>,
+    /// A predefined type for a dynamic value (i.e., one of ‘Discrete’, ‘Step’, ‘Linear’, or ‘Regression’).
+    pub interpolation: Interpolation,
 }
-
 
 /// See [ParametricValues Object -> "interpolation"](https://docs.opengeospatial.org/is/19-045r3/19-045r3.html#tproperties)
 #[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
 pub enum Interpolation {
-    /// The sampling of the attribute occurs such that it is not possible to regard the series as continuous; thus, 
+    /// The sampling of the attribute occurs such that it is not possible to regard the series as continuous; thus,
     /// there is no interpolated value if t is not an element in "datetimes".
     #[default]
     Discrete,
-    /// The values are not connected at the end of a subinterval with two successive instants. The value just jumps from 
+    /// The values are not connected at the end of a subinterval with two successive instants. The value just jumps from
     /// one value to the other at the end of a subinterval.
     Step,
-    /// The values are essentially connected and a linear interpolation estimates the value of the property at the 
+    /// The values are essentially connected and a linear interpolation estimates the value of the property at the
     /// indicated instant during a subinterval.
     Linear,
-    /// The value of the attribute at the indicated instant is extrapolated from a simple linear regression model with 
+    /// The value of the attribute at the indicated instant is extrapolated from a simple linear regression model with
     /// the whole values corresponding to the all elements in "datetimes".
     Regression,
-    /// For a URL, this standard refers to the [InterpolationCode Codelist](http://docs.opengeospatial.org/is/15-042r3/15-042r3.html#75) defined in [OGC TimeseriesML 1.0](http://docs.opengeospatial.org/is/15-042r3/15-042r3.html)[OGC 15-042r3] 
-    /// between neighboring points in a timeseries, e.g., "<http://www.opengis.net/def/timeseries/InterpolationCode/Continuous>", 
+    /// For a URL, this standard refers to the [InterpolationCode Codelist](http://docs.opengeospatial.org/is/15-042r3/15-042r3.html#75) defined in [OGC TimeseriesML 1.0](http://docs.opengeospatial.org/is/15-042r3/15-042r3.html)[OGC 15-042r3]
+    /// between neighboring points in a timeseries, e.g., "<http://www.opengis.net/def/timeseries/InterpolationCode/Continuous>",
     /// "<http://www.opengis.net/def/timeseries/InterpolationCode/Discontinuous>", and etc.
     Url(String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serde_temporal_property() {
+        // https://developer.ogc.org/api/movingfeatures/index.html#tag/TemporalProperty/operation/retrieveTemporalProperty
+        let tp_json = r#"{
+          "name": "speed",
+          "type": "TReal",
+          "form": "KMH",
+          "valueSequence": [
+            {
+              "datetimes": [
+                "2011-07-15T08:00:00Z",
+                "2011-07-15T08:00:01Z",
+                "2011-07-15T08:00:02Z"
+              ],
+              "values": [
+                0,
+                20,
+                50
+              ],
+              "interpolation": "Linear"
+            }
+          ]
+        }"#;
+
+        let _: TemporalProperty =
+            serde_json::from_str(tp_json).expect("Failed to deserialize Temporal Property");
+    }
 }
