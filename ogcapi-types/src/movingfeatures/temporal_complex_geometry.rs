9882cfd07223472a1edf74a0b1f4daf18ceeb377
Matthias Kraus <40459408+KrausMatthias@users.noreply.github.com>
2025-05-25 18:55:55 +0200

    Add a blanket implementation of moving feature geometry transactions to drivers for testing

--- ogcapi-types/src/movingfeatures/temporal_complex_geometry.rs
+++ ogcapi-types/src/movingfeatures/temporal_complex_geometry.rs
@@ -8,32 +8,65 @@ pub enum Type {
     MovingGeometryCollection,
 }
 
-/// A TemporalComplexGeometry object represents a set of TemporalPrimitiveGeometry objects. When a TemporalGeometry 
-/// object has a "type" member is "MovingGeometryCollection", the object is specialized as a TemporalComplexGeometry 
-/// object with one additional mandatory member named "prisms". The value of the "prisms" member is represented by a 
+/// A TemporalComplexGeometry object represents a set of TemporalPrimitiveGeometry objects. When a TemporalGeometry
+/// object has a "type" member is "MovingGeometryCollection", the object is specialized as a TemporalComplexGeometry
+/// object with one additional mandatory member named "prisms". The value of the "prisms" member is represented by a
 /// JSON array of a set of TemporalPrimitiveGeometry instances having at least one element in the array.
 ///
 /// See [7.2.1.2. TemporalComplexGeometry Object](https://docs.ogc.org/is/19-045r3/19-045r3.html#tcomplex)
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub struct TemporalComplexGeometry{
-    r#type: Type,
-    prisms: Prisms,
-    crs: Option<Crs>,
-    trs: Option<Trs>,
+pub struct TemporalComplexGeometry {
+    pub r#type: Type,
+    pub prisms: Prisms,
+    pub crs: Option<Crs>,
+    pub trs: Option<Trs>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(try_from = "PrismsUnchecked")]
 pub struct Prisms(Vec<TemporalPrimitiveGeometry>);
 
+impl Prisms {
+    pub fn new(value: Vec<TemporalPrimitiveGeometry>) -> Result<Self, &'static str> {
+        if !value.is_empty() {
+            Ok(Self(value))
+        } else {
+            Err("Prisms must have at least one value")
+        }
+    }
+
+    pub fn push(&mut self, value: TemporalPrimitiveGeometry) {
+        self.0.push(value)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        // this should never be true
+        self.0.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn try_remove(&mut self, id: &str) -> Result<TemporalPrimitiveGeometry, &'static str> {
+        if self.len() > 2 {
+            self.0
+                .pop_if(|tg| tg.id.as_ref().is_some_and(|tg_id| tg_id == id))
+                .ok_or("Temporal Geometry not found.")
+        } else {
+            Err("Prisms must have at least one value. Try to delete the whole prism.")
+        }
+    }
+}
+
 #[derive(Deserialize, Clone, Debug, PartialEq)]
 struct PrismsUnchecked(Vec<TemporalPrimitiveGeometry>);
 
-impl TryFrom<Vec<TemporalPrimitiveGeometry>> for TemporalComplexGeometry{
+impl TryFrom<Vec<TemporalPrimitiveGeometry>> for TemporalComplexGeometry {
     type Error = &'static str;
 
     fn try_from(value: Vec<TemporalPrimitiveGeometry>) -> Result<Self, Self::Error> {
-        Ok(Self{
+        Ok(Self {
             r#type: Default::default(),
             prisms: PrismsUnchecked(value).try_into()?,
             crs: Default::default(),
@@ -46,11 +79,6 @@ impl TryFrom<PrismsUnchecked> for Prisms {
     type Error = &'static str;
 
     fn try_from(value: PrismsUnchecked) -> Result<Self, Self::Error> {
-        if !value.0.is_empty(){
-            Ok(Prisms(value.0))
-        }else{
-            Err("Prisms must have at least one value")
-        }
+        Self::new(value.0)
     }
 }
-
