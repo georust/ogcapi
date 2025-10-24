6b5b30d91554ec75d8b0359f2c53d6845721f6d7
Balthasar Teuscher <balthasar.teuscher@tum.de>
2025-10-24 18:14:44 +0200

    Running validation

--- examples/cite-service/src/main.rs
+++ examples/cite-service/src/main.rs
@@ -1,19 +1,13 @@
 use clap::Parser;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use ogcapi::{
-    processes::{gdal_loader::GdalLoader, geojson_loader::GeoJsonLoader, greeter::Greeter},
-    services::{AppState, Config, Service},
-};
+use ogcapi::services::{AppState, Config, Service};
 
 #[tokio::main]
 async fn main() {
-    // setup env
-    dotenvy::dotenv().ok();
-
     // setup tracing
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::EnvFilter::new("cite_service=debug,ogcapi=debug,sqlx=warn"))
         .with(tracing_subscriber::fmt::layer().pretty())
         .init();
 
@@ -22,12 +16,6 @@ async fn main() {
 
     // Application state
     let state = AppState::new_from(&config).await;
-
-    // Register processes/processors
-    let state = state.processors(vec![
-        Box::new(Greeter),
-        Box::new(GeoJsonLoader),
-    ]);
 
     // Build & run with hyper
     Service::new_with(&config, state).await.serve().await;
